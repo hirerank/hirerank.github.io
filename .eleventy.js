e11aff4adc06cfe802d77bceef526ae6df70e9a7
naterkane <nater@naterkane.com>
2022-06-09 13:52:36 -0400

    feat(UI): initial wordpress setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css/");
-  eleventyConfig.addWatchTarget("./src/css/");
-  eleventyConfig.addPassthroughCopy("./src/assets");
-  eleventyConfig.addWatchTarget("./src/assets");
+  eleventyConfig.addPassthroughCopy("./ghpages/src/css/");
+  eleventyConfig.addWatchTarget("./ghpages/src/css/");
+  eleventyConfig.addPassthroughCopy("./ghpages/src/assets");
+  eleventyConfig.addWatchTarget("./ghpages/src/assets");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
     dir: {
-      input: "src",
-      output: "docs",
+      input: "ghpages/src",
+      output: "ghpages/dist",
     },
   };
 };
